d1573658d31ebc4c46cf83f2296d4fc7f41bebff
David Chalco <david@chalco.io>
2020-12-17 03:27:25 -0800

    [AUTO][RELEASE]: Bump file header version to "12.0.0"

--- portable/ThirdParty/GCC/Posix/port.c
+++ portable/ThirdParty/GCC/Posix/port.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS Kernel V10.4.2
+ * FreeRTOS Kernel V12.0.0
  * Copyright (C) 2020 Cambridge Consultants Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
