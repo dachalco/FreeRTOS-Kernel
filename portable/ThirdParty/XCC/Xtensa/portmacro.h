8e4d461765627583998c78de438fd43a116ab4ba
David Chalco <david@chalco.io>
2021-05-03 14:40:35 -0700

    [AUTO][RELEASE]: Bump file header version to "10.5.2"

--- portable/ThirdParty/XCC/Xtensa/portmacro.h
+++ portable/ThirdParty/XCC/Xtensa/portmacro.h
@@ -1,6 +1,6 @@
 
 /*
- * FreeRTOS Kernel V10.5.0
+ * FreeRTOS Kernel V10.5.2
  * Copyright (C) 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
